e62b86c72b45759427a5dd5a0c1d7a444effd74a
Johannes Ewald <johannes.ewald@peerigon.com>
2017-09-06 17:53:02 +0200

    Add first shot

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,33 @@
-/* eslint-disable import/unambiguous */
+const webpack = require("webpack");
+const path = require("path");
 
 module.exports = {
     entry: require.resolve("./src/build.js"),
-    output: {},
+    output: {
+        path: path.resolve(__dirname, "benchmark"),
+        filename: "./run.js",
+    },
+    resolve: {
+        alias: {
+            fs: require.resolve("./mocks/dummy"),
+            module: require.resolve("./mocks/dummy"),
+            chokidar: require.resolve("./mocks/chokidar"),
+            "uglify-js": require.resolve("./mocks/dummy"),
+        },
+    },
+    // bail: true,
+    plugins: [
+        // The node-pre-gyp module optionally requires the aws-sdk
+        // It's not necessary for the build though...
+        // new webpack.IgnorePlugin(/^(aws-sdk|child_process)$/, /node_modules[/\\]node-pre-gyp[/\\]lib/),
+        // new webpack.IgnorePlugin(/.*/, /node_modules[/\\]loader-runner[/\\]lib$/),
+        new webpack.ContextReplacementPlugin(/^\.$/, path.dirname(require.resolve("./mocks/dummy.js")), {}),
+        // new webpack.ContextReplacementPlugin(/^\.$/, data => {
+        //     if (/node_modules[/\\]loader-runner[/\\]lib$/.test(data.context) === false) {
+        //         return;
+        //     }
+        //     console.log(data);
+        //     data.request = path.relative(data.context, require.resolve("./mocks/dummy.js"));
+        // }),
+    ],
 };
